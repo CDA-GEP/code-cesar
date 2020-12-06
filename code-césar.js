0ae5404fa4e8eba4184c1e971b1c1e2f1f00cfa1
simplon-marseille <acardnicolas@hotmail.com>
2020-12-06 18:30:18 +0100

    crypto

---
+++ code-césar.js
@@ -0,0 +1,11 @@
+//Code césar - cryptographie
+//Créez un algorithme permettant de crypter une chaine de caractères.
+//Crytpage : décaler chaque lettre un cran vers la droite.
+//
+//ex: chaine non cryptée : avengers
+//    chaine cryptée : bwfohfst
+//
+//courage ! :)
+const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const dataNonCrypte = "avengers";
+
